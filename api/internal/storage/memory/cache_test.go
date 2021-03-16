f81386935e6cf8430fb4a96dd58f85e03240d0ae
koind <krosava_15d@mail.ru>
2021-03-16 22:37:40 +0600

    Добавляю фиктивный репозиторий кэша

---
+++ api/internal/storage/memory/cache_test.go
@@ -0,0 +1,100 @@
+package memory
+
+import (
+	"context"
+	"github.com/koind/cacher/api/internal/domain/repository"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const cacheKey = "test-key"
+
+var cacheRepository repository.CacheRepositoryInterface
+
+func init() {
+	cacheRepository = NewCacheRepository()
+}
+
+func before() {
+	cache := repository.Cache{
+		Kye:   cacheKey,
+		Value: "test-value",
+	}
+
+	cacheRepository.Upsert(context.Background(), cache)
+}
+
+func after() {
+	cacheRepository.Delete(context.Background(), cacheKey)
+}
+
+func TestCacheRepository_Create(t *testing.T) {
+	newCache := repository.Cache{
+		Kye:   cacheKey,
+		Value: "test-value2",
+	}
+
+	_, err := cacheRepository.Upsert(context.Background(), newCache)
+	assert.Nil(t, err, "не должно быть ошибки при создании")
+
+	cache, _ := cacheRepository.GetOneByKey(context.Background(), cacheKey)
+	assert.EqualValues(t, &newCache, cache)
+
+	after()
+}
+
+func TestCacheRepository_Update(t *testing.T) {
+	before()
+
+	newCache := repository.Cache{
+		Kye:   cacheKey,
+		Value: "test-value3",
+	}
+
+	_, err := cacheRepository.Upsert(context.Background(), newCache)
+	assert.Nil(t, err, "не должно быть ошибки при обновлении")
+
+	cache, _ := cacheRepository.GetOneByKey(context.Background(), cacheKey)
+	assert.EqualValues(t, &newCache, cache)
+
+	after()
+}
+
+func TestCacheRepository_GetOneByKey(t *testing.T) {
+	before()
+
+	_, err := cacheRepository.GetOneByKey(context.Background(), cacheKey)
+	assert.Nil(t, err, "не должно быть ошибки при получении записи")
+
+	after()
+
+	_, err = cacheRepository.GetOneByKey(context.Background(), "test-random-key")
+	if assert.NotNil(t, err) {
+		assert.EqualError(t, err, repository.CacheNotFountError.Error(), "ошибки должны совподать")
+	}
+}
+
+func TestCacheRepository_FindAll(t *testing.T) {
+	before()
+
+	list, err := cacheRepository.GetAll(context.Background())
+	assert.NoError(t, err)
+	assert.Len(t, list, 1)
+
+	after()
+
+	list, _ = cacheRepository.GetAll(context.Background())
+	assert.Len(t, list, 0)
+}
+
+func TestCacheRepository_Delete(t *testing.T) {
+	before()
+
+	err := cacheRepository.Delete(context.Background(), cacheKey)
+	assert.Nil(t, err, "не должно быть ошибки при удалении")
+
+	_, err = cacheRepository.GetOneByKey(context.Background(), cacheKey)
+	if assert.NotNil(t, err) {
+		assert.EqualError(t, err, repository.CacheNotFountError.Error(), "ошибки должны совподать")
+	}
+}
---
+++ api/internal/storage/memory/сache.go
@@ -0,0 +1,81 @@
+package memory
+
+import (
+	"context"
+	"github.com/koind/cacher/api/internal/domain/repository"
+	"sync"
+)
+
+// Создает фиктивный репозиторий кэша
+func NewCacheRepository() *CacheRepository {
+	return &CacheRepository{
+		DB: make(map[string]repository.Cache),
+	}
+}
+
+// Фиктивный репозиторий кэша
+type CacheRepository struct {
+	sync.RWMutex
+	DB map[string]repository.Cache
+}
+
+// Обновить запись, если существует, и создает, если нет
+func (r *CacheRepository) Upsert(ctx context.Context, cache repository.Cache) (*repository.Cache, error) {
+	_, err := r.GetOneByKey(ctx, cache.Kye)
+	if err != nil && err != repository.CacheNotFountError {
+		return nil, err
+	}
+
+	r.Lock()
+	defer r.Unlock()
+
+	r.DB[cache.Kye] = cache
+
+	return &cache, nil
+}
+
+// Возвращет одну запись по ключу
+func (r *CacheRepository) GetOneByKey(ctx context.Context, key string) (*repository.Cache, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	cache, has := r.DB[key]
+	if !has {
+		return nil, repository.CacheNotFountError
+	}
+
+	return &cache, nil
+}
+
+// Возвращет все записи
+func (r *CacheRepository) GetAll(ctx context.Context) ([]*repository.Cache, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	if len(r.DB) <= 0 {
+		return nil, nil
+	}
+
+	list := make([]*repository.Cache, 0, len(r.DB))
+
+	for _, cache := range r.DB {
+		list = append(list, &cache)
+	}
+
+	return list, nil
+}
+
+// Удаляет одну запись по ключу
+func (r *CacheRepository) Delete(ctx context.Context, key string) error {
+	_, err := r.GetOneByKey(ctx, key)
+	if err != nil {
+		return err
+	}
+
+	r.Lock()
+	defer r.Unlock()
+
+	delete(r.DB, key)
+
+	return nil
+}
